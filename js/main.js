9fb4cd1d5ac804b0c9307718fcce90b23ffda09a
pppnk <pppnk@hotmail.com>
2019-06-15 23:10:49 -0700

    fix:Lighthouse observations
    Lighthouse observations

--- js/main.js
+++ js/main.js
@@ -50,6 +50,7 @@ fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
     const option = document.createElement('option');
     option.innerHTML = neighborhood;
     option.value = neighborhood;
+    option.role = 'option';
     option.setAttribute('aria-label', `${neighborhood} option`);
     select.append(option);
   });
@@ -80,6 +81,7 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
     option.setAttribute('role', 'option');
     option.innerHTML = cuisine;
     option.value = cuisine;
+    option.role = 'option';
     select.append(option);
   });
 };
